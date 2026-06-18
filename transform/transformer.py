2b2a36f81e8d3c83bbadb97a772d08a79999c954
mounivelaga <mounivelaga@gmail.com>
2026-06-17 23:51:55 -0400

    transformer.py and db_connection.py  code created

---
+++ transform/transformer.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from utils.logger import logger
+
+
+class Transformer:
+    def transform(self, data):
+        logger.info("Transformation Started")
+
+        rows = []
+
+        for cart in data["carts"]:
+            for product in cart["products"]:
+                rows.append(
+                    {
+                        "order_id": cart["id"],
+                        "customer_name": f"Customer_{cart['userId']}",
+                        "product_name": product["title"],
+                        "quantity": product["quantity"],
+                        "unit_price": product["price"],
+                        "total_amount": product["quality"] * product["price"],
+                    }
+                )
+        df = pd.DataFrame(rows)
+
+        logger.info(f"Transformation Completed: {len(df)} rows")
+        return df
